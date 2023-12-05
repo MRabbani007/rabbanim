3d51e248905183b7e720c67fea6db55a154ff698
Mohamad Rabbani <mrabbani@outlook.com>
2023-12-06 04:29:49 +0500

    initial

---
+++ src/data/experience.js
@@ -0,0 +1,69 @@
+import Khazzan from "../assets/png/khazzan.jpg";
+import Ghazeer from "../assets/png/khazzan2.jpg";
+import FGP from "../assets/png/fgp.jpeg";
+import RHIP from "../assets/png/rhip.png";
+import CCC from "../assets/png/ccc.png";
+
+export const experience = [
+  {
+    id: 1,
+    position: "E&I Engineer",
+    company: "Consolidated Contractors Company",
+    project: "FGP 3RD GENERATION PLANT, REPUBLIC OF KAZAKHSTAN",
+    period: "MAR 2020 – JUL 2023",
+    image: FGP,
+    companyLogo: CCC,
+    responsibilities: [
+      "Utilizing advanced work packaging principles to facilitate workplace management and reduce construction costs",
+      "Performing early forecasting of work front and preparing material and tools for smooth flow of work Coordinating with disciplines (Work face planning, engineering, planning and productivity) and client representatives in addition to arranging logistics to request and deliver material to site",
+      "Ensuring that the site is properly staffed to perform all material management duties including material receiving, storage, preservation, inventorying and issuing",
+      "Identifying contractor responsibilities and scopes of supply, preparing material takeoffs, and following on material procurement",
+    ],
+  },
+  {
+    id: 2,
+    position: "Procurement Engineer",
+    company: "Consolidated Contractors Company",
+    project: "GHAZEER CENTRAL PROCESSING FACILITY PHASE II, SULTANATE OF OMAN",
+    period: "AUG 2018 – FEB 2020",
+    image: Ghazeer,
+    companyLogo: CCC,
+    responsibilities: [
+      "Worked on the procurement of permanent material and site consumables including sending requests for quotation, preparing comparison sheets, issuing purchase orders, and following delivery to site",
+      "Coordinated with specialized subcontractors for timely delivery of material and mobilization of manpower",
+      "Worked with engineering department for preparing material takeoffs for E&I",
+    ],
+  },
+  {
+    id: 3,
+    position: "E&I Engineer",
+    company: "Consolidated Contractors Company",
+    project: "RABAB HARWEEL INTEGRATED PROJECT, SULTANATE OF OMAN",
+    period: "JAN 2017 – JUL 2018",
+    image: RHIP,
+    companyLogo: CCC,
+    responsibilities: [
+      "Supervising construction activities including installation of switchgear, cabling, building service systems and general electrical works",
+      "Overseeing project specification, identifying work scope and responsibility matrix, preparing method statements and material approvals",
+      "Preparing material takeoffs and following on material procurement",
+      "Liaising with subcontractors for supply of material and manpower to complete specific jobs",
+      "Preparing progress and productivity reports, managing office documentation and subsystem handover",
+    ],
+  },
+  {
+    id: 4,
+    position: "E&I Engineer",
+    company: "Consolidated Contractors Company",
+    project: "KHAZZAN CENTRAL PROCESSING FACILITY PHASE I, SULTANATE OF OMAN",
+    period: "May 2015 – Dec 2016",
+    image: Khazzan,
+    companyLogo: CCC,
+    responsibilities: [
+      "Supervising construction activities in substations and field to install electrical equipment including HV and LV switchgear, transformers and busducts, UPS system, building HVAC, cable tray and cabling, instrument process control, F&G, and Telecoms",
+      "Leading team to ensure proper execution of work as per drawings, project standards and quality requirements",
+      "Coordinating with other disciplines to ensure readiness of site and complete delivery of work",
+      "Setting delivery schedules, allocating manpower and resources, monitoring work progress and productivity",
+      "Attending client meetings to acquire requisites, report progress and meet mechanical completion requirements",
+    ],
+  },
+];
